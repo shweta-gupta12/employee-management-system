166ce6ca11b20712bd9b171df682647b881e5545
shweta-gupta12 <shwetagkp2210@gmail.com>
2026-06-21 22:36:22 +0530

    Add Project Screesnshot

--- react-frontend-project/src/components/CreateEmployeeComponent.jsx
+++ react-frontend-project/src/components/CreateEmployeeComponent.jsx
@@ -87,7 +87,7 @@ class CreateEmployeeComponent extends Component {
 
     render() {
         return (
-            <div>
+            <div><br/><br/>
                 <div className="container">
                     <div className="row">
                         <div className="card col-md-6 offset-md-3 offset-md-3">
